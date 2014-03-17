35df385b8d22f4b47017e3d56e2ebae04c34a3fa
Jonathan Leibiusky <jleibiusky@gilt.com>
2014-03-17 16:13:02 -0400

    Initial commit

---
+++ src/etcdfs/file.go
@@ -0,0 +1,134 @@
+package etcdfs
+
+import(
+  "github.com/coreos/go-etcd/etcd"
+  "github.com/hanwen/go-fuse/fuse"
+  "github.com/hanwen/go-fuse/fuse/nodefs"
+  "log"
+  "time"
+  "bytes"
+)
+
+type etcdFile struct {
+  etcdClient *etcd.Client
+  path string
+}
+
+func NewEtcdFile(client *etcd.Client, path string) nodefs.File {
+  file := new(etcdFile)
+  file.etcdClient = client
+  file.path = path
+  return file
+}
+
+func (f *etcdFile) SetInode(*nodefs.Inode) {
+}
+func (f *etcdFile) InnerFile() nodefs.File {
+  return nil
+}
+
+func (f *etcdFile) String() string {
+  return "etcdFile"
+}
+
+/*
+func (f *etcdFile) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, child *nodefs.Inode, code fuse.Status) {
+  log.Println("******************************")
+  return nil, nil, fuse.ENOSYS
+}
+*/
+
+func (f *etcdFile) Read(buf []byte, off int64) (fuse.ReadResult, fuse.Status) {
+  res, err := f.etcdClient.Get(f.path, false, false)
+
+  if err != nil {
+    log.Println("Error:", err)
+    return nil, fuse.EIO
+  }
+
+  end := int(off) + int(len(buf))
+  if end > len(res.Node.Value) {
+    end = len(res.Node.Value)
+  }
+
+  data := []byte(res.Node.Value)
+  return fuse.ReadResultData(data[off:end]), fuse.OK
+}
+
+func (f *etcdFile) Write(data []byte, off int64) (uint32, fuse.Status) {
+  res, err := f.etcdClient.Get(f.path, false, false)
+
+  if err != nil {
+    log.Println("Error:", err)
+    return 0, fuse.EIO
+  }
+
+  originalValue := []byte(res.Node.Value)
+  leftChunk := originalValue[:off]
+  end := int(off) + int(len(data))
+
+  var rightChunk []byte
+  if end > len(res.Node.Value) {
+    rightChunk = []byte{}
+  } else {
+    rightChunk = data[int(off)+int(len(data)):]
+  }
+
+  newValue := bytes.NewBuffer(leftChunk)
+  newValue.Grow(len(data)+len(rightChunk))
+  newValue.Write(data)
+  newValue.Write(rightChunk)
+  _, err = f.etcdClient.Set(f.path, newValue.String(), 0)
+
+  if err != nil {
+    log.Println("Error:", err)
+    return 0, fuse.EIO
+  }
+
+  return uint32(len(data)), fuse.OK
+}
+
+func (f *etcdFile) Flush() fuse.Status {
+  return fuse.OK
+}
+
+func (f *etcdFile) Release() {
+}
+
+func (f *etcdFile) GetAttr(out *fuse.Attr) fuse.Status {
+  res, err := f.etcdClient.Get(f.path, false, false)
+
+  if err != nil {
+    log.Println("Error:", err)
+    return fuse.EIO
+  }
+
+  out.Mode = fuse.S_IFREG | 0666
+  out.Size = uint64(len(res.Node.Value))
+  return fuse.OK
+}
+
+func (f *etcdFile) Fsync(flags int) (code fuse.Status) {
+  return fuse.OK
+}
+
+func (f *etcdFile) Utimens(atime *time.Time, mtime *time.Time) fuse.Status {
+  return fuse.ENOSYS
+}
+
+func (f *etcdFile) Truncate(size uint64) fuse.Status {
+  return fuse.OK
+}
+
+func (f *etcdFile) Chown(uid uint32, gid uint32) fuse.Status {
+  return fuse.ENOSYS
+}
+
+func (f *etcdFile) Chmod(perms uint32) fuse.Status {
+  return fuse.ENOSYS
+}
+
+func (f *etcdFile) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+  return fuse.OK
+}
+
---
+++ src/etcdfs/path.go
@@ -0,0 +1,125 @@
+package etcdfs
+
+import(
+  "log"
+  "strings"
+
+  "github.com/hanwen/go-fuse/fuse"
+  "github.com/hanwen/go-fuse/fuse/nodefs"
+  "github.com/hanwen/go-fuse/fuse/pathfs"
+
+  "github.com/coreos/go-etcd/etcd"
+)
+
+type EtcdFs struct {
+  pathfs.FileSystem
+  EtcdEndpoint string
+}
+
+func (me *EtcdFs) NewEtcdClient() *etcd.Client {
+  return etcd.NewClient([]string{me.EtcdEndpoint})
+}
+
+func (me *EtcdFs) Rmdir(name string, context *fuse.Context) (code fuse.Status) {
+  if name == "" {
+    return fuse.OK
+  }
+
+  _, err := me.NewEtcdClient().RawDelete(name, true, true)
+
+  if err != nil {
+    log.Println(err)
+    return fuse.ENOENT
+  }
+
+  return fuse.OK
+}
+
+func (me *EtcdFs) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+  _, err := me.NewEtcdClient().Set(name, "", 0)
+
+  if err != nil {
+    log.Println("Create Error:", err)
+    return nil, fuse.ENOENT
+  }
+
+  return NewEtcdFile(me.NewEtcdClient(), name), fuse.OK
+}
+
+func (me *EtcdFs) Mkdir(name string, mode uint32, context *fuse.Context) fuse.Status {
+  if name == "" {
+    return fuse.OK
+  }
+
+  _, err := me.NewEtcdClient().CreateDir(name, 0)
+
+  if err != nil {
+    log.Println(err)
+    return fuse.ENOENT
+  }
+
+  return fuse.OK
+}
+
+func (me *EtcdFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
+  if name == "" {
+    return &fuse.Attr{
+      Mode: fuse.S_IFDIR | 0666,
+    }, fuse.OK
+  }
+
+  res, err := me.NewEtcdClient().Get(name, false, false)
+
+  if err != nil {
+    return nil, fuse.ENOENT
+  }
+
+  var attr fuse.Attr
+
+  if res.Node.Dir {
+    attr = fuse.Attr{
+      Mode: fuse.S_IFDIR | 0666,
+    }
+  } else {
+    attr = fuse.Attr{
+      Mode: fuse.S_IFREG | 0666, Size: uint64(len(res.Node.Value)),
+    }
+  }
+
+  return &attr, fuse.OK
+}
+
+func (me *EtcdFs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
+  res, err := me.NewEtcdClient().Get(name, false, false)
+
+  if err != nil {
+    log.Println("OpenDir Error:", err)
+    return nil, fuse.ENOENT
+  }
+
+  entries := []fuse.DirEntry{}
+
+  for _, e := range(res.Node.Nodes) {
+    chunks := strings.Split(e.Key, "/")
+    file := chunks[len(chunks)-1]
+    if e.Dir {
+      entries = append(entries, fuse.DirEntry{Name: file, Mode: fuse.S_IFDIR})
+    } else {
+      entries = append(entries, fuse.DirEntry{Name: file, Mode: fuse.S_IFREG})
+    }
+  }
+
+  return entries, fuse.OK
+}
+
+func (me *EtcdFs) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+  _, err := me.NewEtcdClient().Get(name, false, false)
+
+  if err != nil {
+    log.Println("Open Error:", err)
+    return nil, fuse.ENOENT
+  }
+
+  return NewEtcdFile(me.NewEtcdClient(), name), fuse.OK
+}
+
